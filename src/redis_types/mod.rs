7407fb0db884e9e683f8179f13f7b0a46354e464
Gabriel Saiago <grsaiago@gmail.com>
2026-06-09 21:52:05 -0300

    add null strings

--- src/redis_types/mod.rs
+++ src/redis_types/mod.rs
@@ -1,12 +1,14 @@
 mod array;
 mod bulk_string;
 mod integer;
+mod null_strings;
 mod simple_error;
 mod simple_string;
 
 pub use array::ArrayDataType;
 pub use bulk_string::BulkStringDataType;
 pub use integer::IntegerDataType;
+pub use null_strings::NullBulkStringData;
 pub use simple_error::SimpleErrorDataType;
 pub use simple_string::SimpleStringDataType;
 
@@ -15,6 +17,7 @@ use nom::{IResult, Parser, branch::alt};
 #[derive(Debug, PartialEq)]
 pub enum RedisType {
     Array(ArrayDataType),
+    NullBulkString(NullBulkStringData),
     BulkString(BulkStringDataType),
     Integer(IntegerDataType),
     SimpleString(SimpleStringDataType),
@@ -25,6 +28,7 @@ impl RedisType {
     pub fn parse<'a>(input: &'a str) -> IResult<&'a str, RedisType> {
         alt((
             ArrayDataType::parse,
+            NullBulkStringData::parse, // this has to be before the BulkString because They're the same identifier
             BulkStringDataType::parse,
             IntegerDataType::parse,
             SimpleErrorDataType::parse,
---
+++ src/redis_types/null_strings.rs
@@ -0,0 +1,34 @@
+use nom::{IResult, bytes::complete::tag};
+
+use crate::redis_types::RedisType;
+
+#[derive(Debug, PartialEq)]
+pub struct NullBulkStringData;
+
+impl NullBulkStringData {
+    const RESP_IDENTIFIER: &str = "$-1\r\n";
+
+    pub fn new() -> Self {
+        Self
+    }
+
+    pub fn parse<'a>(input: &'a str) -> IResult<&'a str, RedisType> {
+        let (input, _) = tag(Self::RESP_IDENTIFIER)(input)?;
+        Ok((input, RedisType::NullBulkString(NullBulkStringData::new())))
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::redis_types::{NullBulkStringData, RedisType};
+
+    #[test]
+    fn simple_parse() {
+        let input = "$-1\r\n";
+
+        assert_eq!(
+            Ok(("", RedisType::NullBulkString(NullBulkStringData))),
+            NullBulkStringData::parse(input)
+        );
+    }
+}
