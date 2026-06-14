e60c23038d46f6dff0fdeda2763e9f52f2456478
Gabriel Saiago <grsaiago@gmail.com>
2026-06-14 00:07:26 -0300

    refact move RedisType into a separate file

--- src/redis_types/mod.rs
+++ src/redis_types/mod.rs
@@ -1,7 +1,9 @@
 mod array;
 mod bulk_string;
+mod command;
 mod integer;
 mod null_strings;
+mod redis_type;
 mod simple_error;
 mod simple_string;
 
@@ -9,60 +11,6 @@ pub use array::ArrayDataType;
 pub use bulk_string::BulkStringDataType;
 pub use integer::IntegerDataType;
 pub use null_strings::NullBulkStringData;
+pub use redis_type::RedisType;
 pub use simple_error::SimpleErrorDataType;
 pub use simple_string::SimpleStringDataType;
-
-use nom::{IResult, Parser, branch::alt};
-
-#[derive(Debug, PartialEq, Eq)]
-pub enum RedisType {
-    Array(ArrayDataType),
-    NullBulkString(NullBulkStringData),
-    BulkString(BulkStringDataType),
-    Integer(IntegerDataType),
-    SimpleString(SimpleStringDataType),
-    SimpleError(SimpleErrorDataType),
-}
-
-impl RedisType {
-    pub fn parse<'a>(input: &'a str) -> IResult<&'a str, RedisType> {
-        alt((
-            ArrayDataType::parse,
-            NullBulkStringData::parse, // this has to be before the BulkString because They're the same identifier
-            BulkStringDataType::parse,
-            IntegerDataType::parse,
-            SimpleErrorDataType::parse,
-            SimpleStringDataType::parse,
-        ))
-        .parse(input)
-    }
-}
-
-#[cfg(test)]
-mod test {
-    use nom::IResult;
-
-    use crate::redis_types::{NullBulkStringData, RedisType};
-
-    #[test]
-    fn simple_parse() {
-        struct TestCase<'a> {
-            input: &'a str,
-            expected: IResult<&'a str, RedisType>,
-        }
-
-        let cases = [TestCase {
-            input: "$-1\r\n",
-            expected: Ok(("", RedisType::NullBulkString(NullBulkStringData::new()))),
-        }];
-
-        for (i, case) in cases.iter().enumerate() {
-            let result = RedisType::parse(case.input);
-            assert_eq!(
-                result, case.expected,
-                "case number {}, the input was `{}`",
-                i, case.input
-            );
-        }
-    }
-}
---
+++ src/redis_types/redis_type.rs
@@ -0,0 +1,59 @@
+use nom::{IResult, Parser, branch::alt};
+
+use crate::redis_types::{
+    ArrayDataType, BulkStringDataType, IntegerDataType, NullBulkStringData, SimpleErrorDataType,
+    SimpleStringDataType,
+};
+
+#[derive(Debug, PartialEq, Eq)]
+pub enum RedisType {
+    Array(ArrayDataType),
+    NullBulkString(NullBulkStringData),
+    BulkString(BulkStringDataType),
+    Integer(IntegerDataType),
+    SimpleString(SimpleStringDataType),
+    SimpleError(SimpleErrorDataType),
+}
+
+impl RedisType {
+    pub fn parse<'a>(input: &'a str) -> IResult<&'a str, RedisType> {
+        alt((
+            ArrayDataType::parse,
+            NullBulkStringData::parse, // this has to be before the BulkString because They're the same identifier
+            BulkStringDataType::parse,
+            IntegerDataType::parse,
+            SimpleErrorDataType::parse,
+            SimpleStringDataType::parse,
+        ))
+        .parse(input)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use nom::IResult;
+
+    use crate::redis_types::{NullBulkStringData, RedisType};
+
+    #[test]
+    fn simple_parse() {
+        struct TestCase<'a> {
+            input: &'a str,
+            expected: IResult<&'a str, RedisType>,
+        }
+
+        let cases = [TestCase {
+            input: "$-1\r\n",
+            expected: Ok(("", RedisType::NullBulkString(NullBulkStringData::new()))),
+        }];
+
+        for (i, case) in cases.iter().enumerate() {
+            let result = RedisType::parse(case.input);
+            assert_eq!(
+                result, case.expected,
+                "case number {}, the input was `{}`",
+                i, case.input
+            );
+        }
+    }
+}
